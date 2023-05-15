d6cf0c3c83a6879a66e97b13b5ba01da4b513e94
Максим Мазур <1mazurmaxim12@gmail.com>
2023-05-15 15:49:40 +0300

    додав 3 класи

---
+++ City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ_HW15
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public int Population { get; set; }
+        public Country Country { get; set; }
+        public DateTime CityFounded { get; set; }
+    }
+}
